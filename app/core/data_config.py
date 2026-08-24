c45d58c1391842c410e4aab1923e21d0769cba95
mhlohh <rockriderskerala@gmail.com>
2026-08-24 22:40:08 +0530

    pep8 formmated

--- app/core/data_config.py
+++ app/core/data_config.py
@@ -1,17 +1,18 @@
 from app.core.model import OutPass
 import csv
 import os
+
 field_names = list(OutPass.model_fields.keys())
 
 file_name = "student_outpass"
 
 file_path = f"data/{file_name}.csv"
 
-def intialize_data_set(field_names = field_names):
+
+def intialize_data_set(field_names=field_names):
     if os.path.exists(file_path):
         print("File already exist!")
     else:
-        with open(file_path, mode='w', newline='') as file:
-            writer = csv.DictWriter(file, fieldnames=field_names) 
+        with open(file_path, mode="w", newline="") as file:
+            writer = csv.DictWriter(file, fieldnames=field_names)
             writer.writeheader()
-
