0624e93d853b73c8fb9f0484ff9aaf92e3d83837
mhlohh <rockriderskerala@gmail.com>
2026-08-24 22:11:09 +0530

    Update: Build proper update function for updating outpass

--- app/core/data_loader.py
+++ app/core/data_loader.py
@@ -1,15 +1,14 @@
-from app.core.data_config import intialize_data_set,file_path,field_names
+from app.core.data_config import intialize_data_set, file_path, field_names
 from fastapi import HTTPException, status
 from app.core.model import OutPass
 import os
 import csv
 
-
 outpass_dict = {}
 dict_list = []
-#load csv as dictionary
+# load csv as dictionary
 if os.path.exists(file_path):
-    with open(file_path,"r") as file:
+    with open(file_path, "r") as file:
         reader = csv.DictReader(file)
         dict_list = list(reader)
     for item in dict_list:
@@ -20,13 +19,19 @@ else:
     intialize_data_set()
     print("INFO:    The dataset is intialized ☑️")
 
-#convert dict_list to id value pair
+# convert dict_list to id value pair
 
-#Load entire contents in dataset
+
+# Load entire contents in dataset
 def all_outpass_data():
+    print(list(outpass_dict.values()))
+    data = list(outpass_dict.values())
+    for value in data:
+        print(value)
     return outpass_dict
 
-#Load specific Outpass by iud
+
+# Load specific Outpass by iud
 def outpass_id_data(id):
     id = str(id)
     if id in outpass_dict:
@@ -34,17 +39,36 @@ def outpass_id_data(id):
     else:
         print("WARNING:  Item not found ❌")
         raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND, 
-            detail="Item not found"
+            status_code=status.HTTP_404_NOT_FOUND, detail="Item not found"
         )
 
-#Add an Outpass
+
+# Add an Outpass
 def add_outpass_data(outpass: OutPass):
     print(f"INFO:   Outpass Aded Sucessfuly id:{outpass.id} ☑️")
     outpass_dict[outpass.id] = outpass
     data = dict(outpass)
-    with open(file_path, "a", newline='') as file:
-        writer = csv.DictWriter(file,fieldnames=field_names)
+    with open(file_path, "a", newline="") as file:
+        writer = csv.DictWriter(file, fieldnames=field_names)
         writer.writerow(data)
     return outpass
-    
+
+
+# update outpass
+def update_outpass_data(outpass, id):
+    id = str(id)
+    if id not in outpass_dict:
+        print("INFO:     Not found to update")
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"Item with Register ID {id} not Found!",
+        )
+
+    else:
+        outpass_dict[id] = outpass
+        data = [dict(value) for value in outpass_dict.values()]
+        with open(file_path, "w", newline="") as file:
+            writer = csv.DictWriter(file, fieldnames=field_names)
+            writer.writeheader()
+            writer.writerows(data)
+            print(f"INFO:    UPdated Sucessfully id: {id} ☑️")
--- app/main.py
+++ app/main.py
@@ -1,44 +1,42 @@
 from fastapi import FastAPI, HTTPException, status
-from app.core.data_loader import all_outpass_data,outpass_id_data,add_outpass_data
+from app.core.data_loader import all_outpass_data, outpass_id_data, add_outpass_data, update_outpass_data
 from app.core.model import OutPass
 import csv
 import os
+
 app = FastAPI()
 
 lists = {
-
     "4122341": OutPass(
         name="Muhsil NR",
         course="Btech AI/ML",
-        id= "4122341",
+        id="4122341",
         reason="Cycling",
         date="02/08/2026",
         inside="6:30",
-        outside="4:30")
+        outside="4:30",
+    )
 }
+
 
 @app.get("/outpass")
 def outpass():
     return all_outpass_data()
 
+
 @app.get("/outpass/{id}")
-def get_outpass_(id:str):
+def get_outpass_(id: str):
     return outpass_id_data(id)
-  
+
+
 @app.post("/outpass")
-def add_outpass(outpass:OutPass):
+def add_outpass(outpass: OutPass):
     return add_outpass_data(outpass)
 
+
 @app.put("/outpass/{id}")
-def update_outpass(outpass:OutPass, id:str):
-    if id not in lists:
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND,
-            detail=f"Item with Register ID {id} does not exist"
-        )
-    else:
-        outpass.id = id
-        lists[id] = outpass
+def update_outpass(outpass: OutPass, id: str):
+    update_outpass_data(outpass, id)
 
 @app.delete("/outpass/{id}")
 def delete_outpass(id):
@@ -46,4 +44,3 @@ def delete_outpass(id):
         del lists[id]
 
     return "Deleted Sucessfully"
-        
